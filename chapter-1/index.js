0a7c22b8f3ffd8c1e55b134ff71e9df47f01e6f7
sana naz <sn6409074@gmail.com>
2025-08-01 23:52:41 +0500

    commit

---
+++ chapter-1/index.js
@@ -0,0 +1,11 @@
+
+
+alert('Error!please enter your valid password')
+
+alert('welcome to js land happy codding')
+
+alert('Welcome js Land')
+
+alert('happy codding')
+
+alert('Hello...i can learn JS thought My web browser console')
